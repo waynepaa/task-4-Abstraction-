509ae0c9d6f28f5ea81f5f15c39f90d38b9534aa
Jay Villa <jovenjayvilla08@gmail.com>
2025-10-01 16:40:59 +0800

    WIP: updates to main.dart

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,6 +1,7 @@
 import 'payment_method.dart';
 import 'credit_card_payment.dart';
 import 'paypal_payment.dart';
+import 'bank_transfer_payment.dart';
 
 void main() {
   var processor1 = PaymentProcessor(CreditCardPayment());
@@ -8,4 +9,7 @@ void main() {
 
   var processor2 = PaymentProcessor(PayPalPayment());
   processor2.process(50.0);
+
+  var processor3 = PaymentProcessor(BankTransferPayment());
+  processor3.process(200.0);
 }
